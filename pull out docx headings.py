19eccbcbe40ca2ea4f35a22fcb823b139a136979
Ethan Dyer <e.j.dyer@eagle.clarion.edu>
2020-03-25 13:36:34 -0400

    can pull out info out of some documents
    inconsistent.. read comments

--- pull out docx headings.py
+++ pull out docx headings.py
@@ -1,7 +1,19 @@
 from docx import Document
-document2 = Document("Test.docx")
 
+document2 = Document("test1.docx")
 
+underHeading = False
 for par in document2.paragraphs:
-    if(par.style.name.startswith('Heading')):
+    if (underHeading == True):
         print(par.text)
+        underHeading = False
+    if (par.style.name.startswith('Heading') ):
+        underHeading = True
+
+
+
+# code is pretty simple but there are many issues that are headaches:
+# some documents have alot of headings that are random/ not needed
+# making a list of headings that are acceptable and checking them might help, but it also restricts how useful the program is
+# (any doc without any of those headings would not have a summary )
+# in some documents there is and empty section between the heading and the next text block, while in others it is right after
